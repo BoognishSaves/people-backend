b42be755e8a3a93dd7f6cd99003a785a83cb1186
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-09-14 15:02:58 -0600

    Postman Routes Tested

--- express-react/backend/controller/people-controller.js
+++ express-react/backend/controller/people-controller.js
@@ -2,24 +2,45 @@
 
 const express = require('express')
 const router = express.Router()
+const {People} = require('../models')
+// we can use 'object-destructuring' to access just the model we need for this controller 
 
+// ...our routes below
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
 
 // PEOPLE INDEX ROUTE
 router.get("/", async (req, res) => {
-	res.status(200).json({message: "people index route"})
+	try {
+    // get all people
+    res.json(await People.find({}));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
 });
 
 // PEOPLE CREATE ROUTE
-router.post("/", async (req, res) =>  {
-	res.status(201).json({message: "people create route"})
+router.post("/", async (req, res) => {
+  try {
+    // create new person
+    res.json(await People.create(req.body));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
 });
 
 // PEOPLE SHOW ROUTE
 router.get("/:id", async (req, res) => {
-	res.status(200).json({message: "people show route: " + req.params.id })
+    try {
+        // send one person
+        res.json(await People.findById(req.params.id));
+      } catch (error) {
+        //send error
+        res.status(400).json(error);
+      }
 });
 
 // PEOPLE DELETE ROUTE
